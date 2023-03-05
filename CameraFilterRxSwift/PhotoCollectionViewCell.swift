c1e5206c09d265768362ad37141fb988cf4237c7
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-05 16:58:26 +0900

    FEAT: Display Selected Photo

---
+++ CameraFilterRxSwift/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilterRxSwift
+//
+//  Created by BoMin on 2023/03/05.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}

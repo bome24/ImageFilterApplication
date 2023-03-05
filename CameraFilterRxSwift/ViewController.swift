34cfd19bd54623b6eba8cb45f2c8a67488234c0b
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-05 17:44:31 +0900

    FIX: applyFilter to Observable

--- CameraFilterRxSwift/ViewController.swift
+++ CameraFilterRxSwift/ViewController.swift
@@ -43,13 +43,14 @@ class ViewController: UIViewController {
             return
         }
         
-        FilterService().applyFilter(to: sourceImage) { filteredImage in
-            
-            DispatchQueue.main.async {
-                self.photoImageView.image = filteredImage
-            }
-            
-        }
+        FilterService().applyFilter(to: sourceImage)
+            .subscribe(onNext: { filteredImage in
+                
+                DispatchQueue.main.async {
+                    self.photoImageView.image = filteredImage
+                }
+                
+            }).disposed(by: disposeBag)
         
     }
     
